aecacb071ba6d747217dfba355daef18910e2441
razcona <razcona3@gmail.com>
2024-03-12 00:58:01 -0400

    Completed 2 and 3 of the Odin JavaScript exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let appendedStr = '';
+    for (let i = 0; i < num; i++) {
+        appendedStr += string;
+    }
 
+    return appendedStr;
+ 
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join('');
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+console.log(reverseString("message"));
