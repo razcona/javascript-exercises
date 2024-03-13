4a0ee75f0017b586cfd6659f1fbca80693b82da8
razcona <razcona3@gmail.com>
2024-03-13 01:03:56 -0400

    Completed 6 and 7 of the Odin Project JavaScript exercises.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 === 0 && year % 100 != 0) {
+        return true;
+    }
+
+    else if (year % 100 === 0 && year % 400 === 0) {
+        return true; 
+    }
+
+    return false;
 
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+
+//Divisble by 4
+//Not divisible by 100 unless it is divisible by 400
+//
+//
+
+
