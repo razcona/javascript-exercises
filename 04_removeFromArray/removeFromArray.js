dbd62df9681bd317ebff5a125e94698eae54a2db
razcona <razcona3@gmail.com>
2024-03-12 23:36:32 -0400

    Completed 4 and 5 of the Odin project JavaScript exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    let currentIndex;
+    while(true) {
+            for (let item of items) {
+                currentIndex = array.indexOf(item);
+                if (currentIndex != -1)
+                    array.splice(currentIndex, 1);
+            }
+        if (currentIndex === -1) break;
+    } 
+    return array;
 
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < - 0 || typeof start != 'number' || typeof end != 'number') {
+        return 'ERROR';
+    }
+    let sum = 0;
+    if (start <= end) {
+    for(let i = start; i <= end; i++) {
+        sum += i;
+      }
+    }
+    else 
+        for(let i = end; i <= start; i++){
+        sum += i;
+      }
+        
+    return sum;
 
 };
 
