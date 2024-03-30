6f127c7dc8b4f384dc377a19c94c6cd792b1fb52
razcona <razcona3@gmail.com>
2024-03-29 20:33:10 -0400

    Completed exercise 8 of the Odin javascript exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,35 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+    return a - b;
 	
 };
 
-const subtract = function() {
+const sum = function(array) {
+    return array.reduce((total, item) => total + item, 0);
+
+};
+
+const multiply = function(array) {
+    if (!array){
+        return 0;
+    }
+    return array.reduce((total, item) => total * item, 1);
+
+};
+
+const power = function(a, b) {
+    return a ** b;
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(n) {
+    if (n === 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
 	
 };
 
