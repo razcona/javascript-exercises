f53fd3a75f729b8164d0956afa0bc18adaa4731f
razcona <razcona3@gmail.com>
2024-03-30 22:36:28 -0400

    Completed exercise 11-getTheTitles of the Odin Javascript exercises
    Completed exercise 12-findTheOldest of the Odin Javascript exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const sortedPeople = people.sort((first, second) => {
+        if (!first.yearOfDeath || !second.yearOfDeath) {
+            return -1 * (first.yearOfBirth - second.yearOfBirth);
+        }
+
+
+        return (first.yearOfDeath - first.yearOfBirth) - (second.yearOfDeath - second.yearOfBirth);
+        
+    });
+
+    console.log(sortedPeople);
+
+
+    return sortedPeople[sortedPeople.length-1];
 
 };
 
